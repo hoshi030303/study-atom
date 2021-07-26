fb077cd965d3ee7500f096310aeaedc9d27800a5
hoshi030303 <68181024+hoshi030303@users.noreply.github.com>
2021-07-26 21:20:04 +0900

    study global state

--- src/components/molecules/user/UserIconWithName.jsx
+++ src/components/molecules/user/UserIconWithName.jsx
@@ -1,7 +1,11 @@
 import styled from "styled-components";
+import React, { useContext } from "react";
+import { UserContext } from "../../../providers/UserProvider";
 
 export const UserIconWithName = (props) => {
-  const { image, name, isAdmin } = props;
+  const { image, name } = props;
+  const { userInfo } = useContext(UserContext);
+  const isAdmin = userInfo ? userInfo.isAdmin : false;
   return (
     <SContainer>
       <SImage height={160} width={160} src={image} alt={name} />
